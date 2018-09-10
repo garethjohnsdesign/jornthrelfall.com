0e5de927b6463a5923d7d0a53f44826a2d33d186
Gareth Johns <garethjohns@Gareths-iMac.local>
2018-09-10 12:20:31 +0100

    Initial Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,9 @@
-$(document).foundation();
+$(document).on('turbolinks:load', function() {
 
-// 1. Loading
+// 1. Foundation
 // --------------------
 
-/*
-$(function() {
-if (sessionStorage.getItem('set') === 'set') {
-  $('.loader').hide();
-} else {
-  $('.loader').show();
-  setTimeout(function(){
-        $('.loader').addClass('loaded');
-    }, 1500);  
-  sessionStorage.setItem('set', 'set');
-}
-});
-*/
+$(document).foundation();
 
 // 2. Animate on Scroll
 // --------------------
@@ -33,48 +21,4 @@ window.addEventListener('load', AOS.refresh);
 });
 
 
-// 3. Lightgallery
-// ---------------
-
-/*
-$('#video').lightGallery({
-    counter: false,
-    youtubePlayerParams: {
-        modestbranding: 1,
-        showinfo: 0,
-        rel: 0,
-        controls: 0
-    },
-    vimeoPlayerParams: {
-        byline : 0,
-        portrait : 0,
-        color : 'FF8300'     
-    }
-}); 
-*/
-
-
-/*
-$('.home__narrative').hover(function() {
-     $('.home__narrative').addClass('home__narrative--active');
-     $('.home__commercial').addClass('home__commercial--active');
-     $('.navigation').addClass('navigation--active');
-}, function() {
-     $('.home__narrative').removeClass('home__narrative--active');
-     $('.home__commercial').removeClass('home__commercial--active')
-     $('.navigation').removeClass('navigation--active');
 });
-
-$('.home__commercial').hover(function() {
-     $('.home__narrative').removeClass('home__narrative--active');
-     $('.home__commercial').removeClass('home__commercial--active')
-     $('.navigation').removeClass('navigation--active');
-}, function() {
-       $('.home__narrative').addClass('home__narrative--active');
-     $('.home__commercial').addClass('home__commercial--active');
-     $('.navigation').addClass('navigation--active');
-});
-*/
-
-
-
